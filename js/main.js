e127e34150e05611abd1661c347321656c336f93
jayciemota <63515675+jayciemota@users.noreply.github.com>
2021-06-25 15:32:23 -0400

    Add fullPage.js
    Got fullPage.js to work

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var myFullpage = new fullpage('#fullpage', {
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    anchors: ['home', 'projects', 'about', 'contact'],
+    navigation:true,
+    navigationTooltips: ['home', 'projects', 'about', 'contact'],
+    showActiveTooltip: true,
+    menu: '#menu'
+});
