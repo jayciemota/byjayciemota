ecb7d2c3108e0e92cd5aa48fc3dd4fbbd240a14a
jayciemota <63515675+jayciemota@users.noreply.github.com>
2021-07-12 22:43:28 -0400

    MAJOR UPDATE
    Added fully functional navigation menu + some responsive styles to home page and font change to headings.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-var myFullpage = new fullpage('#fullpage', {
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    anchors: ['home', 'projects', 'about', 'contact'],
-    navigation:true,
-    navigationTooltips: ['home', 'projects', 'about', 'contact'],
-    showActiveTooltip: true,
-    menu: '#menu'
-});
+
+
+/* Colorful click effect */
+const COLORS = ['red','green','pink','blue','yellow','purple'];
+
+function clickEffect(e){
+  let rippleColor = Math.floor(Math.random() * COLORS.length);
+  
+  let ripple = document.createElement('div');
+  ripple.className = ('clickRipple');
+  ripple.style.top = `${e.clientY}px`;
+  ripple.style.left = `${e.clientX}px`;
+  ripple.style.borderColor = COLORS[rippleColor];
+  document.body.appendChild(ripple);
+  
+  ripple.addEventListener('animationend',()=>{
+    ripple.parentElement.removeChild(ripple);
+  })
+}
+document.addEventListener('click', clickEffect)
+
+/* Navigation Menu Toggle */
+const menuToggle = document.querySelector('a.menu-toggle')
+const navMenu = document.querySelector('.overlay')
+
+document.addEventListener('click', function () {
+  const clickEd = menuToggle.contains(event.target)
+
+ if (clickEd) {
+  if (menuToggle.classList.contains('clicked')) {
+    navMenu.classList.add('slide-out-top')
+    navMenu.classList.remove('bounce-in-top')
+    menuToggle.classList.toggle('clicked') 
+    } 
+   else {
+    navMenu.classList.remove('slide-out-top')
+    navMenu.classList.add('bounce-in-top')
+    menuToggle.classList.toggle('clicked') 
+    }
+} 
+  else {
+   menuToggle.classList.remove('clicked')
+    navMenu.classList.remove('bounce-in-top')
+}
+})
